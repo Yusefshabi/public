1cd1c50017d7e278cb3de05df72520b4fda8dbd4
Yusefshabi <124084297+Yusefshabi@users.noreply.github.com>
2024-08-30 02:01:55 +0400

    Update server.js

--- server.js
+++ server.js
@@ -8,12 +8,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Serve static files (HTML, CSS, JS)
+// Serve static files (like CSS, JS, images)
 app.use(express.static(path.join(__dirname)));
 
 // Route to serve the main landing page
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'html for landing page.html'));
+    res.sendFile(path.join(__dirname, 'html for landing page.html')); 
 });
 
 // POST endpoint for suggestions
